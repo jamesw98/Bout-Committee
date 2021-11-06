55dfeb4adea782dd646dc0563824d3d3d557abe8
jamesw98 <xeekatar@gmail.com>
2021-11-06 16:49:42 -0400

    first 2 db interactions done

--- api/db_interactions.js
+++ api/db_interactions.js
@@ -1,43 +1,84 @@
-const sqlite3 = require("sqlite3").verbose();
+const DB = require("better-sqlite3")("../db/bout_committee.db");
 
-let DB = new sqlite3.Database("../db/bout_committee.db", (err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    else {
-        console.log("Connected to db");
-    }
-});
-
+/**
+ * runs an sql query with the .run() 
+ * @param {*} sql the sql query
+ * @returns the result of the .run() call
+ */
 function run_sql(sql) {
-    DB.run(sql, [], (err, rows) => {
-        if (err) {
-            console.log(err);
-            return err;
-        }
-        return rows;
-    });
+    let statement = DB.prepare(sql);
+    return statement.run();
 }
 
+/**
+ * runs an sql query with the .get() function
+ * @param {*} sql the sql query 
+ * @returns the result of the .get() call
+ */
+function get_sql(sql) {
+    let statement = DB.prepare(sql);
+    return statement.get();    
+}
+
+/**
+ * add a fencer to the Fencers table with minimal information known about the fencer
+ * @param {*} first_name the first name of the fencer
+ * @param {*} last_name  the last name of the fencer
+ * @param {*} division   the division the fencer fences in
+ * @returns a string result
+ */
 function create_fencer_minimal(first_name, last_name, division) {
+    // check if there is a fencer with this name and division 
     let sql = `
+        SELECT * 
+        FROM 
+            Fencers
+        WHERE
+            FirstName = '${first_name}' and 
+            LastName = '${last_name}' and
+            Division = '${division}'
+    `
+
+    let exists = get_sql(sql);
+    
+    // if a fencer already exists with this name and division, do not accept
+    if (exists) {
+        return "Fencer already exists with that name in that division! Please include a USFA membership number."
+    }
+
+    // create a new fencer in the Fencers table
+    sql = `
         INSERT INTO Fencers (FirstName, LastName, Division)
             VALUES ('${first_name}', '${last_name}', '${division}')
     `;
 
-    run_sql(sql);
+    let result = run_sql(sql)
+
+    if (result.changes) {
+        return `Fencer, ${first_name} ${last_name} created successfully`
+    }
+    return `An error occurred, please contact an administrator`
 }
 
+/**
+ * update a value for a fencer in the Fencers table
+ * @param {*} fencer_id the ID for the fencer
+ * @param {*} key the column to be updated
+ * @param {*} value the value to be set
+ * @returns a string result
+ */
 function update_fencer_value(fencer_id, key, value) {
     let sql = `
-        UPDATE Fencers f
+        UPDATE Fencers
         SET
-            ${key} = ${value}
+            ${key} = '${value}'
         WHERE
             pkFencer = ${fencer_id}
     `;
 
-    run_sql(sql);
-}
+    let result = run_sql(sql);
 
-create_fencer_minimal("Test", "Fencer", "GA");
+    if (result.changes) {
+        return `Successfully updated ${key} to ${value}`
+    }
+}
